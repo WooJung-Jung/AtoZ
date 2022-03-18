106c976a4d9e4cc2d50bdad21f85b58fc889347f
kr-moon <ezmazinga@gmail.com>
2022-03-18 17:40:05 +0900

    feat: Develop Employee & EmployeeInfo

---
+++ project/src/project/Employee.java
@@ -0,0 +1,15 @@
+package project;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class Employee {
+    private Integer employeeNum;
+    private String firstName;
+    private String lastName;
+    private String cl;
+    private String phoneNum;
+    private Date birthday;
+    private String certi;
+}
