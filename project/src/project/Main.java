72ac3d2eabdffae80067e5ca64cb7cc8d62dbcda
User <User@LAPTOP-I34KMO80>
2022-03-22 12:09:07 +0900

    feat: 메인에 기능 연계 및 버그 수정
    20220322
    CJH

--- project/src/project/Main.java
+++ project/src/project/Main.java
@@ -3,15 +3,39 @@ package project;
 import java.util.ArrayList;
 
 public class Main {
-	public static void main(String[] args) {
-		
+	public static void main(String[] args) throws Exception {
+
 		ArrayList<String> input = new ArrayList<String>();
-		
+
 		FileIOManager ioManager = new FileIOManager();
 		input = ioManager.read("input.txt");
-		
-		Parser parser = new Parser();
-		ArrayList<CmdInfo> cmd = parser.parse(input);
 
+		Parser parser = new Parser();
+		ArrayList<CmdInfo> cmds = parser.parse(input);
+
+		EmployeeInfo employeeInfo = new EmployeeInfo();
+		ArrayList<String> output = new ArrayList<String>();
+
+		for (CmdInfo cmd : cmds) {
+			EmployeeInfoMgr employeeInfoMgr = new EmployeeInfoMgr();
+
+			if ("ADD".equals(cmd.command)) {
+				employeeInfoMgr.add(employeeInfo.getInfoTable("employeeNum"), cmd.commandData);
+			} 
+			else if ("DEL".equals(cmd.command)) {
+				SearchOptionBuilder searchOptionBuilder = new SearchOptionBuilder(cmd.commandData.get(0).column, cmd.option.opt2, "");
+				output.add(Employee.printAll(employeeInfoMgr.delete(employeeInfo.getInfoTable("employeeNum"), searchOptionBuilder.getSearcher(), cmd.commandData)));
+			} 			
+			else if ("SCH".equals(cmd.command)) {
+				SearchOptionBuilder searchOptionBuilder = new SearchOptionBuilder(cmd.commandData.get(0).column, cmd.option.opt2, "");
+				output.add(Employee.printAll(employeeInfoMgr.search(employeeInfo.getInfoTable("employeeNum"), searchOptionBuilder.getSearcher(), cmd.commandData)));
+			} 
+			else if ("MOD".equals(cmd.command)) {
+				SearchOptionBuilder searchOptionBuilder = new SearchOptionBuilder(cmd.commandData.get(0).column, cmd.option.opt2, "");
+				output.add(Employee.printAll(employeeInfoMgr.modify(employeeInfo.getInfoTable("employeeNum"), searchOptionBuilder.getSearcher(), cmd.commandData)));
+			}
+
+			ioManager.write("output.txt", output);
+		}
 	}
 }
--- project/src/project/MonthBirthSearchStrategy.java
+++ project/src/project/MonthBirthSearchStrategy.java
@@ -9,7 +9,7 @@ public class MonthBirthSearchStrategy implements SearchStrategy{
         ArrayList<Employee> employees = new ArrayList<Employee>();
         for (Integer item : table.keySet()) {
             if (target.equals((table.get(item).getBirthday().getMonth()))) {
-                employees.add(employees.get(item));
+                employees.add(table.get(item));
             }
         }
         return employees;
